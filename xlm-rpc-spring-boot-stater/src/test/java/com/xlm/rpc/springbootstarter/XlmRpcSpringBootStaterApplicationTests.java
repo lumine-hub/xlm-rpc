9eb3accffd8fd1d529a1a4b221ca58456a37f6b4
lumine <2761151386@qq.com>
2024-07-15 15:59:19 +0800

    创建spring-boot项目初始化

---
+++ xlm-rpc-spring-boot-stater/src/test/java/com/xlm/rpc/springbootstarter/XlmRpcSpringBootStaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlm.rpc.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlmRpcSpringBootStaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
